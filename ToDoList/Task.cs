bebf98612e8cd35367aa2505a4f3460dea2b0b88
EthanMartino <martino9448@student.cptc.edu>
2020-04-10 12:09:00 -0700

    Documented the Task class and its properties

--- ToDoList/Task.cs
+++ ToDoList/Task.cs
@@ -5,14 +5,29 @@ using System.Threading.Tasks;
 
 namespace ToDoList
 {
+    /// <summary>
+    /// Represents a single Task in a ToDoList
+    /// </summary>
     public class Task
     {
+        /// <summary>
+        /// Identity column for the Database
+        /// </summary>
         public int TaskId { get; set; }
 
+        /// <summary>
+        /// The name of the Task
+        /// </summary>
         public string TaskTitle { get; set; }
 
+        /// <summary>
+        /// A description for the Task
+        /// </summary>
         public string Description { get; set; }
 
+        /// <summary>
+        /// The expected Date for the Task to be finished
+        /// </summary>
         public DateTime DueDate { get; set; }
     }
 }
