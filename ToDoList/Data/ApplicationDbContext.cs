875cf1c8c6c6e2b4e8ef201a3356fb33c99f44bd
KarloParagas <karloparagas@gmail.com>
2020-04-10 12:48:50 -0700

    Added Task and List DbSets and also foreign key relations

--- ToDoList/Data/ApplicationDbContext.cs
+++ ToDoList/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
 
 namespace ToDoList.Data
 {
@@ -11,6 +12,19 @@ namespace ToDoList.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<List> Lists { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<List>() //Dependent entity
+                        .HasOne<Task>() //Parent Entity
+                        .WithMany()
+                        .HasForeignKey(task => task.TaskId); //The foreign key that you want to set up as from the Task.cs class
         }
     }
 }
