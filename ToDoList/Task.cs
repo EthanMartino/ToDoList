d356e5dcb681be12318ff8beb13350bd779f88f9
EthanMartino <martino9448@student.cptc.edu>
2020-04-10 12:24:28 -0700

    Added DataAnnotations for Task class

--- ToDoList/Task.cs
+++ ToDoList/Task.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,24 +11,28 @@ namespace ToDoList
     /// </summary>
     public class Task
     {
+        [Key, Required]
         /// <summary>
         /// Identity column for the Database
         /// </summary>
         public int TaskId { get; set; }
 
+        [StringLength(50), Required, Display(Name = "Task")]
         /// <summary>
         /// The name of the Task
         /// </summary>
         public string TaskTitle { get; set; }
 
+        [StringLength(200)]
         /// <summary>
         /// A description for the Task
         /// </summary>
         public string Description { get; set; }
 
+        [StringLength(20), Display(Name = "Expected Completion")]
         /// <summary>
         /// The expected Date for the Task to be finished
         /// </summary>
-        public DateTime DueDate { get; set; }
+        public string DueDate { get; set; }
     }
 }
