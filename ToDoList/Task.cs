ee17c2098700c7fc88424e12d6e8015b25acec18
EthanMartino <martino9448@student.cptc.edu>
2020-04-10 12:06:38 -0700

    Created Task class and added properties

---
+++ ToDoList/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+
+        public string TaskTitle { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DueDate { get; set; }
+    }
+}
