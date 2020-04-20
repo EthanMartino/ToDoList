89e87de9c32e356f26128f77fb50727baf3a2ea7
KarloParagas <karloparagas@gmail.com>
2020-04-20 16:31:38 -0700

    Changed IdentityUser property to a regular UserId property

--- ToDoList/Models/TDList.cs
+++ ToDoList/Models/TDList.cs
@@ -19,19 +19,10 @@ namespace ToDoList.Models
         public int ListId { get; set; }
 
         /// <summary>
-        /// Foreign Key to IdentityUser Id
-        /// Note: Since this is a froeign key from an Identity, 
-        /// it auto creates the migration with this property as a foreign key.
-        /// No need to do the modelBuilder to make it a foreign key.
+        /// Keeps track of the logged in user's id
         /// </summary>
-        [Required]
-        public IdentityUser Id { get; set; }
-
-        /// <summary>
-        /// Foreign key to Task model
-        /// </summary>
-        [Required]
-        public int TaskId { get; set; }
+        [StringLength(450)]
+        public string UserId { get; set; }
 
         /// <summary>
         /// Title/Category of the todo list
