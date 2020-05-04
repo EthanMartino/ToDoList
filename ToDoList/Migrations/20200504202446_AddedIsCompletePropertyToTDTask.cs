771cd159208c531219d4920fb810419bf9fa4532
EthanMartino <martino9448@student.cptc.edu>
2020-05-04 13:26:01 -0700

    Added Migration for isCompleted property in TDTask

---
+++ ToDoList/Migrations/20200504202446_AddedIsCompletePropertyToTDTask.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoList.Migrations
+{
+    public partial class AddedIsCompletePropertyToTDTask : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isCompleted",
+                table: "TDTasks",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isCompleted",
+                table: "TDTasks");
+        }
+    }
+}
